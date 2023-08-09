3fc98ffcf9065f5a326870d7900f0b3ed138eed9
Micc <50607849+miaomiaocatmsg@users.noreply.github.com>
2023-08-10 04:00:54 +0800

    Create hifimusic.py

---
+++ hifimusic.py
@@ -0,0 +1,49 @@
+'''
+cron: 0 0 0 * * *
+new Env('hifini音乐网 签到');
+
+'''
+
+
+
+
+import os
+import requests
+import json
+
+
+HIFI_COOKIE=os.getenv("HIFI_COOKIE")
+
+
+def sign():
+    hifiurl = "https://hifini.com/sg_sign.htm"
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+    'Cookie': HIFI_COOKIE,
+    'Host': 'hifini.com',
+    'Connection': 'keep-alive',
+    'Accept': 'text/plain, */*; q=0.01',
+    'Referer': 'https://hifini.com/sg_sign.htm'
+    }
+
+
+    response = requests.post(url=hifiurl, headers=headers)
+    result = json.loads(response.text)
+    print("签到结果：",result['message'])
+        
+
+
+
+if HIFI_COOKIE is None:
+    print("未获取到 hifini音乐网 Cookies 请重新获取")
+else:
+    print("已获取到 hifini音乐网 Cookies 准备开始签到 ")
+    sign()
+
+
+
+
+
+
+
+
