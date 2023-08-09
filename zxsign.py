6e14d900c156d86c33f90d52116d5145aba4cd0b
DavM <50607849+miaomiaocatmsg@users.noreply.github.com>
2023-08-09 23:38:15 +0800

    Update zxsign.py

--- zxsign.py
+++ zxsign.py
@@ -4,13 +4,7 @@ import requests
 
 ZX_COOKIES=os.getenv("ZX_COOKIE")
 
-if ZX_COOKIES is None:
-    print("未获取到 知轩藏书 Cookies 请重新获取")
-else:
-    print("已获取到 知轩藏书 Cookies 准备开始签到 ")
-    sign()
-    
-    
+
 
 def sign():
     zxurl = "https://zxcstxt.com/wp-admin/admin-ajax.php"
@@ -27,6 +21,13 @@ def sign():
 
     response = requests.post(url=zxurl, headers=headers,data=data)
     print(response.text)
+
+
+if ZX_COOKIES is None:
+    print("未获取到 知轩藏书 Cookies 请重新获取")
+else:
+    print("已获取到 知轩藏书 Cookies 准备开始签到 ")
+    sign()
 
 
 
