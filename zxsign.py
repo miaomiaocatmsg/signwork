d253223759a1b12257aa13519027e1e8d2296b15
DavM <50607849+miaomiaocatmsg@users.noreply.github.com>
2023-08-09 23:20:32 +0800

    Update zxsign.py

--- zxsign.py
+++ zxsign.py
@@ -1,4 +1,17 @@
 import os
-print(os.getenv("ZX_COOKIE"))
+
+
+ZX_COOKIES=os.getenv("ZX_COOKIE")
+
+if ZX_COOKIES is None:
+    print("未获取到 知轩藏书 Cookies 请重新获取")
+else:
+    print("已获取到 知轩藏书 Cookies 准备开始签到 ")
+    
+    
+
+
+
+
 
 
