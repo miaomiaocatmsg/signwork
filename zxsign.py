181dac8465fd89cfb26be04befd3825918cd296a
DavM <50607849+miaomiaocatmsg@users.noreply.github.com>
2023-08-09 23:32:44 +0800

    Update zxsign.py

--- zxsign.py
+++ zxsign.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 
 ZX_COOKIES=os.getenv("ZX_COOKIE")
@@ -9,6 +10,25 @@ else:
     print("已获取到 知轩藏书 Cookies 准备开始签到 ")
     
     
+
+def sign():
+    zxurl = "https://zxcstxt.com/wp-admin/admin-ajax.php"
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+    'Cookie': ZX_COOKIES,
+    'Host': 'zxcstxt.com',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Referer': 'https://zxcstxt.com/'
+    }
+    data = {'action': 'user_checkin'}
+
+    response = requests.post(url=zxurl, headers=headers,data=data)
+    print(response.text)
+
+
+
 
 
 
