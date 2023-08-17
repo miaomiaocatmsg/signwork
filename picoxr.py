91c34dbbfbb85173a43e77ef8f640e011f2f6690
Micc <50607849+miaomiaocatmsg@users.noreply.github.com>
2023-08-18 00:13:49 +0800

    Update picoxr.py
    新增 每日访问

--- picoxr.py
+++ picoxr.py
@@ -34,7 +34,23 @@ def sign():
     else:
         print("本次签到成功 签到积分：",jf)
 
-   
+def dailyVisit():
+    url="https://bbs.picoxr.com/ttarch/api/growth/v1/user/online?app_id=264482&service_id=0&lang=zh-Hans-CN"
+    headers={
+        'Host':'bbs.picoxr.com',
+        'Connection':'keep-alive',
+        'User-Agent':'Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/115.0.0.0Safari/537.36',
+        'Content-Type':'application/json',
+        'Accept':'application/json,text/plain,*/*',
+        'Origin':'https://bbs.picoxr.com',
+        'Referer':'https://bbs.picoxr.com/?is_new_connect=0&is_new_user=0',
+
+    }
+
+    response = requests.post(url=url, headers=headers,)
+    result = json.loads(response.text)
+    print("每日访问任务：",result["data"])
+
 
 
 
@@ -42,7 +58,15 @@ if PICOXR_COOKIE is None:
     print("未获取到 PicoXR网 Cookies 请重新获取")
 else:
     print("已获取到 PicoXR网 Cookies 准备开始签到 ")
+    dailyVisit()
     sign()
+
+
+
+
+
+
+
 
 
 
