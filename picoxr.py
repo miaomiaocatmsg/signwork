b41b606f54f5132f42ce55b2a53d19f98ef9fa16
jack <i.orz.abc@gmail.com>
2023-09-27 23:40:28 +0800

    1、新增每日访问

--- picoxr.py
+++ picoxr.py
@@ -52,7 +52,19 @@ def dailyVisit():
     result = json.loads(response.text)
     print("每日访问任务：",result["data"])
 
-
+def dailyShare():
+    url="https://bbs.picoxr.com/ttarch/api/growth/v1/user/share?app_id=264482&service_id=0&lang=zh-Hans-CN"
+    headers={
+        'Host':'bbs.picoxr.com',
+        'Connection':'keep-alive',
+        'Accept':'application/json,text/plain,*/*',
+        'User-Agent':'Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/117.0.0.0Safari/537.36',
+        'Referer':'https://bbs.picoxr.com/post/7281503888166862905',
+        'Cookie': PICOXR_COOKIE,
+    }
+    response = requests.post(url=url, headers=headers,)
+    result = json.loads(response.text)
+    print("每日分享任务：",result["data"])
 
 
 if PICOXR_COOKIE is None:
@@ -61,6 +73,7 @@ else:
     print("已获取到 PicoXR网 Cookies 准备开始签到 ")
     dailyVisit()
     sign()
+    dailyShare()
 
 
 
